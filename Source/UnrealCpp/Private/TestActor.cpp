aa7fd0f9b873199820fee4e77978de7fe2472b32
Leedohyeon63 <dhanrfk27@gmail.com>
2025-11-07 11:59:24 +0900

    uclass

--- Source/UnrealCpp/Private/TestActor.cpp
+++ Source/UnrealCpp/Private/TestActor.cpp
@@ -7,21 +7,41 @@
 ATestActor::ATestActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
 // Called when the game starts or when spawned
 void ATestActor::BeginPlay()
 {
-	Super::BeginPlay();
-	
+	Super::BeginPlay();//부모 클래스의 beginplay를 실행
+	UE_LOG(LogTemp, Log, TEXT("Hello 한글도 가능") );
+	UE_LOG(LogTemp, Warning, TEXT("Hello 한글도 가능"));
+	UE_LOG(LogTemp, Error, TEXT("Hello 한글도 가능") );
+}
+
+void ATestActor::Test_NativeFunctoin_Implementation()
+{
+	UE_LOG(LogTemp, Log, TEXT("NativeFuntion호출"));
+
 }
 
 // Called every frame
 void ATestActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+void ATestActor::Testnormal()
+{
+	UE_LOG(LogTemp, Log, TEXT("Normal호출"));
+
+}
+
+void ATestActor::Test_UFunction()
+{
+	UE_LOG(LogTemp, Log, TEXT("UFuntion호출"));
 
 }
 
--- Source/UnrealCpp/Public/TestActor.h
+++ Source/UnrealCpp/Public/TestActor.h
@@ -4,8 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+//generated 헤더는 모든 헤더 중 가장 마지막에 있어야 함
 #include "TestActor.generated.h"
-
+//Ucalss
+//c++클래스를 언리얼 엔진에 객체 시스템에 등록
+//UFUNCTION
+//블루프린트에서 함수를 호출, 구현, 재정의 등을 할 때 사용
+//델리게이트에 바인딩 할 경우 
 UCLASS()
 class UNREALCPP_API ATestActor : public AActor
 {
@@ -18,6 +23,15 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	void Testnormal();
+	UFUNCTION(BlueprintCallable, Category = "Function")
+	void Test_UFunction();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Function")
+	void Test_ImplementableFunctoin();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Function")
+	void Test_NativeFunctoin();
 
 public:	
 	// Called every frame
