5fd830df684065fcf0444b2a206b3055f6208917
Leedohyeon63 <dhanrfk27@gmail.com>
2025-11-24 20:05:31 +0900

    데미지 팝업

---
+++ Source/UnrealCpp/Private/DamgeWidget.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DamgeWidget.h"
+#include "Components/TextBlock.h"
+
+void UDamgeWidget::SetDamageText(float Damage)
+{
+	if (DamageText)
+	{
+		DamageText->SetText(FText::AsNumber(static_cast<int32>(Damage)));
+	}
+}
+
+void UDamgeWidget::PlayPopupAnimation()
+{
+	if (PopupAnimation)
+	{
+		PlayAnimation(PopupAnimation);
+	}
+}
