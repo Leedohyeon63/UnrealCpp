b48481c3d8e2e1a9826c3f6a3015707898377a42
Leedohyeon63 <dhanrfk27@gmail.com>
2025-11-28 13:47:59 +0900

    인벤토리 구현중

--- Source/UnrealCpp/Private/Item/PickUpItem.cpp
+++ Source/UnrealCpp/Private/Item/PickUpItem.cpp
@@ -2,4 +2,16 @@
 
 
 #include "Item/PickUpItem.h"
+#include "Player/InventoryOwner.h"
 
+void APickUpItem::OnPickUpComolete_Implementation()
+{
+	if (PickupOwner.IsValid() && PickupOwner->Implements<UInventoryOwner>())
+	{
+		IInventoryOwner::Execute_AddItem(PickupOwner.Get(), PickupItem, ItemCount);
+		UE_LOG(LogTemp, Log, TEXT("APickupWeapon::OnPickupComplete_Implementation2"));
+
+	}
+
+	Super::OnPickUpComolete_Implementation();
+}
--- Source/UnrealCpp/Public/Item/PickUpItem.h
+++ Source/UnrealCpp/Public/Item/PickUpItem.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Item/PickUp.h"
+#include "Common/CommonEnum.h"
 #include "PickUpItem.generated.h"
 
 /**
@@ -13,5 +14,13 @@ UCLASS()
 class UNREALCPP_API APickUpItem : public APickUp
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void OnPickUpComolete_Implementation() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PickUp|Item")
+	EItemCode PickupItem = EItemCode::BasicWeapon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pickup|Item")
+	int32 ItemCount = 1;
 };
